71e8de256993ba9caeed8de49ab0bb8a6a02e390
IzzyCraze365 <johnaisabella3@gmail.com>
2023-04-03 17:04:55 -0400

    Initial Commit for Unit 3

---
+++ arrayMethods.js
@@ -0,0 +1,60 @@
+// Unit 3 - Day 011
+// Array Methods
+
+// Convert strings to arrays based of a space.
+let words = "I really have been enjoying this coding camp!";
+let wordArr = words.split(" ");
+console.log(wordArr);
+
+// Opposite of a Split is a Join
+//converts an array into a sentence
+let wordJoin = wordArr.join(" ");
+console.log(wordJoin);
+
+// The push method adds an element to the ending of an array
+let studentNames = ["Trevor", "John"];
+studentNames.push("Ashlee");
+console.log(studentNames);
+
+// .pop() will remove the last item in the array
+let poppedStudents = studentNames.pop();
+console.log(studentNames);
+console.log(poppedStudents);
+studentNames.push("Simon");
+
+// .slice() returns selected elements in an array and returns a new array
+// you can giv eit a start and stop point or omit the stop point
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 3); // Starts at position 1 ends at position 3
+console.log(fruits);
+console.log(citrus);
+const noBanana = fruits.slice(1); //Whole array minus the first input
+console.log(noBanana);
+
+// .splice() it will add or remove to the array element
+//fruits.splice(1,1); // Takes out the 1st position element and the 1 after it
+console.log(fruits); //returns [ 'Banana', 'Lemon', 'Apple', 'Mango' ]
+fruits.splice(1, 2);
+console.log(fruits); // returns [ 'Banana', 'Mango' ]
+fruits.splice(2, 0, "Strawberry", "Grape"); //add them after position 2
+console.log(fruits);
+
+// .reverse() reverses the order of the array
+let numberArr = [1, 2, 3, 4, 5, "winner"];
+console.log(numberArr.reverse()); // prints [ 'winner', 5, 4, 3, 2, 1 ]
+
+// .includes() method check if the array includes certain values
+// returns true or false
+
+const nums = [1, 2, 3, 41, 5, 141, 4];
+
+const includesThree = nums.includes(3);
+console.log(includesThree); // returns true
+
+const includesFortyOne = nums.includes(41);
+console.log(includesFortyOne); // returns false
+
+// . indexOf() it will search the array for a specific element and returns the first index.  If it doesn't find anything it returns a -1/
+
+console.log(nums.indexOf(4)); // position 6
+console.log(nums.indexOf(10)); // position -1 (does not exit)
---
+++ arrays.js
@@ -0,0 +1,92 @@
+// Unit 3 - Day 011
+
+/* 
+ARRAYS
+Arrays are objects that have characteristics
+- Arrays are resizable and can have a mix of different data types
+- Arrays are indexed at zero
+- Example of Datatypes: string, boolean, number, objects, arrays, functions, undefined, null
+
+-What makes arrays powerful is they are loopable
+*/
+
+// ? How do we create an array
+// Arrays consist of an opening and closing of the square brackets ([])
+const names = ["George", "Tom", "Beth", "JoAnn"]; //<-- Prefered way of creating arrays
+
+const namesArrayConstructor = new Array("George", "Tom", "Beth", "JoAnn");
+
+// ? What does it mean to have indexes?
+//                 0          1       2
+let cityArr = ["Houston", "Tampa", "Alanta"];
+
+// ? How Do I Access "Houston"
+console.log(cityArr[0]);
+// ? How Do I Access "Alanta"
+console.log(cityArr[2]);
+
+// ? How Do I Get the Size of the Array?
+console.log("cityArr Length", cityArr.length);
+
+// ? How do I loop through an array
+
+for (let i = 0; i < cityArr.length; i++) {
+  console.log(cityArr[i]);
+}
+// ! Arrays can have mixed data types
+//  Level 1:           0       1   2          3
+//  Level 2:                              0   1   2
+let mixedDataArr = ["Spider", 65, true, [23, 55, 10]];
+
+// ? How do I access "Spider"
+
+console.log("spider: ", mixedDataArr[0]);
+
+// ? How do I access the 55
+console.log("55: ", mixedDataArr[3][1]);
+
+// ? How do I know the size of the array [23, 55, 10]
+console.log("mixedDataArr level2 Array Size: ", mixedDataArr[3].length);
+
+// ? How to we replace a value in the array
+
+let studentNames = ["Trevor", "John"];
+// Replace John with Simon
+studentNames[1] = "Simon";
+
+// ? How do we add on to the array
+// Add Ashlee to the end of the array
+
+studentNames.push("Ashlee");
+console.log(studentNames);
+
+// Create a loop that will go through the array of groceryItemPrices
+//  Inside the loop add on to the array of groceryItemPricesWithTax = itemPrice *1.06 .
+
+let groceryItemPrices = [29.99, 1.5, 8.59, 45.89];
+
+let groceryItemPricesWithTax = []; // An Empty array
+for (let i = 0; i < groceryItemPrices.length; i++) {
+  let itemPriceWithTax = groceryItemPrices[i] * 1.06;
+  groceryItemPricesWithTax.push(+itemPriceWithTax.toFixed(2)); //toFixed() Rounds to decimal place in numebrs // Tge + turns the string into a number
+}
+
+console.log(groceryItemPricesWithTax);
+
+// Arrays are Objects
+
+// ! For Of Loop can be done with Arrays
+/* 
+1. a variable that will be used as reference to which index it is currently on. 
+Typically it is the singluar version of the array (ex: restaurantNames would be restrauntName)
+2. Keyword of - it's needed to complete the For - of loop
+3. Array that you will be looping over
+
+*/
+let groceryItemPricesWithTax2 = [];
+//     1             2        3
+for (groceryItemPrice of groceryItemPrices) {
+  let itemPriceWithTax = groceryItemPrice * 1.06;
+  groceryItemPricesWithTax2.push(itemPriceWithTax);
+}
+console.log(groceryItemPricesWithTax2);
